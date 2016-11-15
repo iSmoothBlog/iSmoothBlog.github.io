bdab732ec9e2d04ea5082d02f01f1ddfbfd271e7
Saleem Almajed <info@ismoothblog.com>
2016-11-15 06:03:20 +0300

    Fix sticky widget script

--- js/sticky-widget.js
+++ js/sticky-widget.js
@@ -1,12 +1,12 @@
 $(function() {
-    var ks_widget_top = $(&#39;#HTML2&#39;).offset().top;
+    var ks_widget_top = $('#HTML2').offset().top;
     var ks_sticky_widgets = function(){
         var ks_current_top = $(window).scrollTop();
            
         if (ks_current_top &gt; ks_widget_top) {
-		$(&#39;#HTML2&#39;).css({ &#39;position&#39;: &#39;fixed&#39;, &#39;top&#39;:20, &#39;width&#39;:300 });
+		$('#HTML2').css({ 'position': 'fixed', 'top':20, 'width':300 });
         } else {
-            $(&#39;#HTML2&#39;).css({ &#39;position&#39;: &#39;static&#39; }); 
+            $('#HTML2').css({ 'position': 'static' }); 
         }  
     };
     ks_sticky_widgets();
