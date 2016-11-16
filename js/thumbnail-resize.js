c4b01e7efa78b00cc46c654a4a21091de3d1fe01
Saleem Almajed <info@ismoothblog.com>
2016-11-16 07:40:39 +0300

    Adjust thumbnail js resize value

--- js/thumbnail-resize.js
+++ js/thumbnail-resize.js
@@ -1,7 +1,7 @@
 function bp_thumbnail_resize(image_url,post_title)
 {
-var image_width=200;
-var image_height=150;
+var image_width=240;
+var image_height=180;
 image_tag='<img width="'+image_width+'" height="'+image_height+'" src="'+image_url.replace('/s72-c/','/w'+image_width+'-h'+image_height+'-c/')+'" alt="'+post_title.replace(/"/g,"")+'" title="'+post_title.replace(/"/g,"")+'"/>';
 if(post_title!="") return image_tag; else return ""; 
 }
