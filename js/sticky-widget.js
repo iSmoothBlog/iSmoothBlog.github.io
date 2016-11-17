5c7d68c325fb29030f048db7ae62e87db5585650
Saleem Almajed <info@ismoothblog.com>
2016-11-18 02:37:02 +0300

    Fix sticky widget JS

--- js/sticky-widget.js
+++ js/sticky-widget.js
@@ -2,6 +2,8 @@ $(function() {
     if ($("#HTML2").length) {
         var o = $("#HTML2"),
             t = $("#HTML2").offset().top,
+        $(window).scroll(function() {
+            var f = $(window).scrollTop();
             i = $("#HTML2").height();
             if (f > t ? o.css({
                     position: "fixed",
