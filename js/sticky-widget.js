e467839f929d5c2db76c7570ccf3ec94fe7f01c6
Saleem Almajed <info@ismoothblog.com>
2016-11-18 02:32:20 +0300

    Sticky widget adjustments

--- js/sticky-widget.js
+++ js/sticky-widget.js
@@ -3,12 +3,10 @@ $(function() {
         var o = $("#HTML2"),
             t = $("#HTML2").offset().top,
             i = $("#HTML2").height();
-        $(window).scroll(function() {
-            var s = $("#footer-wrapper").offset().top - i - 20,
-                f = $(window).scrollTop();
             if (f > t ? o.css({
                     position: "fixed",
-                    top: 20
+                    top: 20,
+					width:: "auto"
                 }) : o.css("position", "static"), f > s) {
                 var n = s - f;
                 o.css({
