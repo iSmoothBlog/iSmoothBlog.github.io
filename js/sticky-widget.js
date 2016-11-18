030dcbf6471ef2fc593f46d639c0cd07cb6a63cd
Saleem Almajed <info@ismoothblog.com>
2016-11-18 03:09:24 +0300

    update

--- js/sticky-widget.js
+++ js/sticky-widget.js
@@ -4,7 +4,7 @@ $(function() {
             t = $("#HTML2").offset().top,
             i = $("#HTML2").height();
         $(window).scroll(function() {
-            var s = $("#footer-wrapper").offset().top - i - 700,
+            var s = $("#footer-wrapper").offset().top - i - 900,
                 f = $(window).scrollTop();
             if (f > t ? o.css({
                     position: "fixed",
