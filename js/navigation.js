d1c525b69a78a91030082d5750542f793a2f8823
ismoothblog <info@ismoothblog.com>
2016-12-04 20:15:17 +0300

    update

--- js/navigation.js
+++ js/navigation.js
@@ -41,7 +41,7 @@
                 if (currentPage == "page") {
                     html += '<span class="displaypageNum prevpage"><a href="#" onclick="redirectpage(' + prevNumber + ');return false">' + prevText + '</a></span>'
                 } else {
-                    html += '<span class="displaypageNum prevpage"><a href="/search/label/' + postLabel + '?&max-results=' + perPage + '">' + prevText + '</a></span>'
+                    html += '<span class="displaypageNum prevpage"><a href="#" onclick="redirectlabel(' + prevNumber + ');return false">' + prevText + '</a></span>'
                 }
             } else {
                 if (currentPage == "page") {
