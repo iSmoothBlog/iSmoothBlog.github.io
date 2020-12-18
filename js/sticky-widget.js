b970993b12dd06e81deb29e9b4f44d628d164624
iSmoothBlog <info@ismoothblog.com>
2020-12-18 15:35:55 +0300

    Update sticky-widget.js

--- js/sticky-widget.js
+++ js/sticky-widget.js
@@ -4,7 +4,7 @@ $(function () {
             t = $("#HTML1").offset().top,
             i = $("#HTML1").height();
         $(window).scroll(function () {
-            var s = $("#blog-pager").offset().top - i - 300,
+            var s = $("#blog-pager").offset().top - i - 1000,
                 f = $(window).scrollTop();
             if (
                 (f > t
