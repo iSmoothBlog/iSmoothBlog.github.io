f9ad0b69f9d93e22fd9103f16836f65c2b7db1fb
iSmoothBlog <info@ismoothblog.com>
2020-12-18 15:03:46 +0300

    Update misc.js

--- js/misc.js
+++ js/misc.js
@@ -67,3 +67,35 @@ for (var pres = document.querySelectorAll("pre,code,blockquote,i"), i = 0; i < p
         },
         !1
     );
+
+// Sticky Widget
+
+$(function () {
+    if ($("#HTML1").length) {
+        var o = $("#HTML1"),
+            t = $("#HTML1").offset().top,
+            i = $("#HTML1").height();
+        $(window).scroll(function () {
+            var s = $("#blog-pager").offset().top - i - 1000,
+                f = $(window).scrollTop();
+            if (
+                (f > t
+                    ? o.css({
+                          position: "fixed",
+                          width: 300,
+                          top: 20,
+                      })
+                    : o.css({
+                          position: "relative",
+                          top: 0,
+                      }),
+                f > s)
+            ) {
+                var n = s - f;
+                o.css({
+                    top: n,
+                });
+            }
+        });
+    }
+});
